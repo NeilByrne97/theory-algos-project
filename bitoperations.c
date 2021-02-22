26b7c92f2ecd9f0adb6b16f5a6497bebffd6611f
Neil Byrne <G00343624@gmit.ie>
2021-02-22 15:49:05 +0000

    Created bitoperations class

---
+++ bitoperations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define WORD unit32_t
+#define PF PRIX32
+
+WORD Ch(WORD x, WORD y, WORD z){
+
+}
+
+int main(int argc, char *argv[]){
+
+    WORD x = 0x0F0F0F0F;
+    WORD y = 0x0A0A0A0A
+    WORD z = 0xB0B0B0B0
+}
