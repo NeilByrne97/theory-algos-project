5a0908614a39fdaf1574fffd1dc87214bbaaa3b3
Neil Byrne <G00343624@gmit.ie>
2021-02-22 16:27:47 +0000

    Added Maj function

--- bitoperations.c
+++ bitoperations.c
@@ -8,15 +8,21 @@ WORD Ch(WORD x, WORD y, WORD z){
     return (x&y)^(~x&z);
 }
 
+WORD Maj(WORD x, WORD y, WORD z){
+    return (x&y)^(x&z)^(y&z);
+}
+
 int main(int argc, char *argv[]){
 
     WORD x = 0x0F0F0F0F;
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
-    WORD ans = Ch(x, y, z);
+    WORD ansCh = Ch(x, y, z);
+    WORD ansMaj = Maj(x, y, z);
 
-    printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
+    printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ansCh);
+    printf("Maj(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ansMaj);
     return 0; 
 
 }
